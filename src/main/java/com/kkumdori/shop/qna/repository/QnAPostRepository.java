9be9849f7d6ce6f08fe13acd3d339fc77f01419a
chs112100 <chs112100@gmail.com>
2025-01-03 09:05:15 +0900

    중복 파일 삭제

--- src/main/java/com/kkumdori/shop/qna/repository/QnAPostRepository.java
+++
@@ -1,8 +0,0 @@
-package com.kkumdori.shop.qna.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.kkumdori.shop.qna.entity.QnAPost;
-
-public interface QnAPostRepository extends JpaRepository<QnAPost, Integer> {
-}
